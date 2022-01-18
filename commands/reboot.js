cd9469c9e3eb88aa62fee38e886e55b877aa7bb8
AntonCatple <Antosha969@gmail.com>
2022-01-18 17:34:43 +0300

    Update reboot.js
    Added shutdown function (reboot)
    Fixed reboot and logout timings

--- commands/reboot.js
+++ commands/reboot.js
@@ -11,7 +11,8 @@ class reboot {
 		this.perms = [""];
 		this.category = "Utils";
 		this.args = "";
-		this.advargs = "<-q> - быстрый березапуск именно бота, а не всей системы";
+		this.advargs = "<-q> - быстрый березапуск именно бота, а не всей системы\n" +
+					   "<-s> - выключение (использовать только в крайнем случае)";
 		this.usage = "";
         this.desc = "перезапуск бота hide";
         this.advdesc = "Полный перезапуск бота\n\n**Доступна только основателю!**";
@@ -19,10 +20,29 @@ class reboot {
     }
 
     run(client, msg, args){
+		function getTimestamp() {
+			var date = new Date();
+			var hours = date.getHours()
+			if (hours < 10) {
+				hours = "0" + hours
+			}
+			var mins = date.getMinutes()
+			if (mins < 10) {
+				mins = "0" + mins
+			}
+			var seconds = date.getSeconds()
+			if (seconds < 10) {
+				seconds = "0" + seconds
+			}
+			var result = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + hours + ":" + mins + ":" + seconds
+			return result
+		}
 		let executable_comm = 'reboot'
 		if (args[1]) {
 			if (args[1] == '-q') {  
 				executable_comm = 'pm2 restart all'
+			} else if (args[1] == '-s') {
+				executable_comm = 'shutdown'
 			} else {
 				let embed = new Discord.MessageEmbed()
 				embed.setTitle(client.user.username + ' - reboot')
@@ -31,17 +51,27 @@ class reboot {
 				msg.channel.send({ embeds: [embed] });
 			}
 		}
-		console.log("rebooting system...");
 		let embed = new Discord.MessageEmbed()
 		embed.setTitle(client.user.username + ' - reboot')
 		embed.setColor(`#F36B00`)
-		embed.setDescription(":arrows_counterclockwise: Перезапускаем систему...")
+		if (executable_comm != 'shutdown') {
+			embed.setDescription(":arrows_counterclockwise: Перезапуск...")
+		} else {
+			embed.setDescription(":mobile_phone_off: Выключение...")
+		}
+		//embed.setFooter({text: 'Executing "' + executable_comm + '"...'})
 		msg.channel.send({ embeds: [embed] });
-		client.user.setActivity('Перезагрузка...');
+		client.user.setActivity('Пока!');
 		client.user.setStatus('idle')
 		setTimeout(() => {
+			console.log(getTimestamp() + ' [INFO] Logging out of Discord...')
 			client.destroy()
-			exec(executable_comm)
+		},1500);
+		
+		setTimeout(() => {
+			console.log(getTimestamp() + ' [INFO] Executing "' + executable_comm + '" and closing node process')
+			await exec(executable_comm)
+			process.exit(0)
 		},5000);
 	}
 }
